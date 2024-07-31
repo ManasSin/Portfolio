7dbffa64c10cfbc088d8fecb3b507b68f589ba12
ManasSin <maltisadan12@gmail.com>
2024-08-01 02:18:31 +0530

    :art: FEAT: Add NavbarSlef component and update page layout
    This commit adds the NavbarSlef component to the UI components folder and updates the page layout in the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Banner from "@/components/Banner";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { Navbar } from "@/components/ui/Navbar";
+import NavbarSlef from "@/components/ui/NavbarSlef";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      {/* <Navbar className="" /> */}
+      <NavbarSlef />
       <main className="flex flex-col items-center justify-start overflow-hidden mx-auto sm:px-10 px-5  dark:bg-black-100">
         <div className="max-w-6xl w-full">
           <Hero />
