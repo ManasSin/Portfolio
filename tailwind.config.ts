6d397aa818c6298fdd7d795426741eacf5221035
ManasSin <maltisadan12@gmail.com>
2024-12-17 07:18:28 +0530

    :construction: REF: formatting dong using prettier.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const svgToDataUri = require("mini-svg-data-uri");
 
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
